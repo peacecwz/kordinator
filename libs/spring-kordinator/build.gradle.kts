b27cc75f205114eb641f146a350bab92f2c939a4
Baris Ceviz <baris@ceviz.dev>
2024-04-03 21:28:19 +0200

    fix: spring-boot publishing

--- libs/spring-kordinator/build.gradle.kts
+++ libs/spring-kordinator/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm")
+    id("maven-publish")
+    id("signing")
 }
 
 object Versions {
@@ -22,4 +24,54 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("io.mockk:mockk:${Versions.MOCKK_VERSION}")
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.peacecwz"
+            artifactId = "spring-3x-kordinator"
+            version = (System.getenv("LIBRARY_VERSION") ?: "0.0.1").replace("/", "")
+
+            from(components["java"])
+
+            pom {
+                name.set("Kordinator")
+                description.set("A simple coroutine-based coordinator library")
+                url.set("https://maven.pkg.github.com/peacecwz/kordinator")
+                packaging = "jar"
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/peacecwz/kordinator/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("peacecwz")
+                        name.set("Baris Ceviz")
+                        email.set("baris@ceviz.dev")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/peacecwz/kordinator.git")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/peacecwz/kordinator")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("SIGNING_KEY"), System.getenv("SIGNING_PASSWORD"))
+    sign(publishing.publications)
 }
