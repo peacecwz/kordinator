a3638a2a2c7ab6cda4eb65a6c4468b813e971cd3
Baris Ceviz <baris@ceviz.dev>
2024-04-04 15:04:11 +0200

    fix: clean up publishing objects

--- core/kordinator/build.gradle.kts
+++ core/kordinator/build.gradle.kts
@@ -21,4 +21,50 @@ dependencies {
 java {
     withSourcesJar()
     withJavadocJar()
+
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "kordinator"
+            groupId = "io.github.peacecwz"
+            version = project.findProperty("LIBRARY_VERSION")?.toString() ?: "0.1.0"
+            description = "Kordinator is simple and lightweight mediator library with Kotlin coroutines supports"
+
+            pom {
+                name.set("Kordinator")
+                version = project.findProperty("LIBRARY_VERSION")?.toString() ?: "0.1.0"
+                description.set("Kordinator is simple and lightweight mediator library with Kotlin coroutines supports")
+                url.set("https://github.com/peacecwz/kordinator")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("peacecwz")
+                        name.set("Baris Ceviz")
+                        email.set("baris@ceviz.dev")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("SIGNING_KEY"), System.getenv("SIGNING_PASSWORD"))
+    sign(publishing.publications)
 }
--- libs/spring-kordinator/build.gradle.kts
+++ libs/spring-kordinator/build.gradle.kts
@@ -30,4 +30,50 @@ dependencies {
 java {
     withSourcesJar()
     withJavadocJar()
+
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "spring-3x-kordinator"
+            groupId = "io.github.peacecwz"
+            version = project.findProperty("LIBRARY_VERSION")?.toString() ?: "0.1.0"
+            description = "Kordinator is simple and lightweight mediator library with Kotlin coroutines supports"
+
+            pom {
+                name.set("spring-3x-kordinator")
+                version = project.findProperty("LIBRARY_VERSION")?.toString() ?: "0.1.0"
+                description.set("Kordinator is simple and lightweight mediator library with Kotlin coroutines supports")
+                url.set("https://github.com/peacecwz/kordinator")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("peacecwz")
+                        name.set("Baris Ceviz")
+                        email.set("baris@ceviz.dev")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("SIGNING_KEY"), System.getenv("SIGNING_PASSWORD"))
+    sign(publishing.publications)
 }
